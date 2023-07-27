3e2b490119bf3624689b8c1e342ad6f2b767d03d
zhengweix <zheng.wei@famewell.com>
2023-07-26 23:54:00 -0700

    added 230726

---
+++ code/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Wei Zheng on 2023/7/26.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
