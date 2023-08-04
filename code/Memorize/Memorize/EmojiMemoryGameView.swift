fdf1c7b8a8c41a613735d510b6f947dea088726f
zhengweix <zheng.wei@famewell.com>
2023-08-04 00:26:52 -0700

    added 230804

---
+++ code/Memorize/Memorize/EmojiMemoryGameView.swift
@@ -0,0 +1,18 @@
+//
+//  EmojiMemoryGameView.swift
+//  Memorize
+//
+//  Created by Wei Zheng on 2023/8/3.
+//
+
+import SwiftUI
+
+struct EmojiMemoryGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmojiMemoryGameView()
+}
