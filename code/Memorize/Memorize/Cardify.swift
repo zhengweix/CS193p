f1f258cbeefd867f7088525c4e67a7ba4e591871
zhengweix <zheng.wei@famewell.com>
2023-08-08 17:24:43 -0700

    updated 230808

---
+++ code/Memorize/Memorize/Cardify.swift
@@ -0,0 +1,18 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by Wei Zheng on 2023/8/8.
+//
+
+import SwiftUI
+
+struct Cardify: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Cardify()
+}
